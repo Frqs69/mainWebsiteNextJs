0b47ae228d303719751a5813bfe450608c678c54
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-07-26 20:42:52 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,10 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>| KiwiCode</title>
+				<title>| KiwiCode.pl - Frontend Developer</title>
 				<meta
 					name='description'
-					content='Explore the impressive portfolio of a skilled Frontend Developer specializing in React, Next.js, and Tailwind CSS. Witness stunning web projects that showcase cutting-edge design, seamless user experiences, and flawless functionality. Enhance your online presence with this SEO-optimized portfolio, featuring a range of dynamic applications and responsive websites. Discover the power of modern web development, brought to life by this talented developer. Visit now for inspiration and collaboration opportunities.'
+					content='Explore the impressive portfolio of a skilled Frontend Developer specializing in React, Next.js, and Tailwind CSS. Witness stunning web projects that showcase cutting-edge design, seamless user experiences, and flawless functionality'
 				/>
 				<meta
 					name='keywords'
